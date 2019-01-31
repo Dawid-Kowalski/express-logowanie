5e4a51cc13c2a7869a0d75998d06e9fd2ab702a6
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-31 18:01:50 +0100

    Add classes to login form

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/img'));
+
 app.get('/', function (req, res) {
 	res.render('login-template');
 })
